0b4e46b975ef8fec638b271289e9e87ee749fa14
Ashish Rai <mandakinirai2410@gmail.com>
2022-12-13 15:36:51 +0530

    message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//        obj.name = "Mandy";
+//        'name' has private access in 'com.driver.RWOnly'
+        obj.setName("Mandy");
+        System.out.println(obj.getName());
+    }
 }
